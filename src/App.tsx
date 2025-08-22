c299b3b14f88e06b1858c38770439f4f314a3387
Madhur N Patel <124025679+pmadhurn@users.noreply.github.com>
2025-08-22 13:04:48 +0530

    connected vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
-import { Analytics } from "@vercel/analytics/next";
 
 const queryClient = new QueryClient();
 
